73547d19f86a6373ea32e4d916536e66b2d4d960
Pedro Figueira <pe.fogueyra@gmail.com>
2023-11-09 16:35:36 -0300

    add gitignore and adding validations and secondary retry when importing the feed

--- includes/import-rss.php
+++ includes/import-rss.php
@@ -1,12 +1,8 @@
 <?php
-function httfox_get_rss($rss_url, $limit) {
-  $posts_per_page = is_numeric($limit) ? $limit : 0;
-
-  $rss = simplexml_load_file($rss_url);
-  $arr_rss = array();
-  $cont = 0;
-  
+function httfox_get_rss_posts($rss, $posts_per_page) {
   if ($rss) {
+    $arr_rss = array();
+    $cont = 0;
     $namespace = $rss->getNamespaces(true)['media'];
     foreach ($rss->channel->item as $item) {
 
@@ -27,6 +23,25 @@ function httfox_get_rss($rss_url, $limit) {
     }
 
     return $arr_rss;
+  }
+
+  return null;
+}
+
+function httfox_get_rss($rss_url, $limit) {
+  $posts_per_page = is_numeric($limit) ? $limit : 0;
+
+  $simple_rss = simplexml_load_file($rss_url);
+  if ($simple_rss) {
+    return httfox_get_rss_posts($simple_rss, $posts_per_page);
+  }
+  
+  if (class_exists('SimpleXMLElement')) {
+    $get_rss = file_get_contents($rss_url);
+    if ($get_rss) {
+      $rss_per_class = new SimpleXMLElement($get_rss);
+      return httfox_get_rss_posts($rss_per_class, $posts_per_page);
+    }
   }
 
   return null;
